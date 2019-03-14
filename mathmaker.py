7d4ff37febeb44afba835f8d7027d1fcaa8b5224
furryman015 <48392433+furryman015@users.noreply.github.com>
2019-03-14 13:47:38 -0400

    mathmaker.py

---
+++ mathmaker.py
@@ -0,0 +1,6 @@
+import random
+print("Mathmaker V1.0")
+print("What is the highest number for the coefficent?")
+high = input()
+print("Your equation is: ")
+print(random.randint(0,high) + "x" + "(" + random.randint(0,high) + "x" + "+" + random.randint(0,high) + ")"
